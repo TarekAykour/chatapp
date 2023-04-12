6656ab7b47ed3fed76075e9ab576307b4cfb970e
TarekAykour <73133367+TarekAykour@users.noreply.github.com>
2023-04-12 20:23:21 +0200

    chatapp redis

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -154,7 +154,7 @@ CHANNEL_LAYERS = {
         'BACKEND': "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
             # change 127 to the server of the webapp
-            "hosts": [("127.0.0.1", 6379)]
+            "hosts": [("red-cgrfd4rk9u56e3mi5p00", 6379)]
         }
         }
     }
