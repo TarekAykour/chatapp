597f34f1847cc08f883b5bb95f93daa4f006f41e
TarekAykour <73133367+TarekAykour@users.noreply.github.com>
2023-04-15 12:52:51 +0200

    mved the settings module in asgi.py

--- chatapp/asgi.py
+++ chatapp/asgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp.settings')
+
 import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -15,7 +18,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 import backend.route
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp.settings')
+
 
 
 
