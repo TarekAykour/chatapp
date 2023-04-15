62bdc12467329ca5b357005e36b13c840ddce9c0
TarekAykour <73133367+TarekAykour@users.noreply.github.com>
2023-04-15 13:41:26 +0200

    changed asgi again...

--- chatapp/asgi.py
+++ chatapp/asgi.py
@@ -8,8 +8,9 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
+import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp.settings')
+
 
 import django
 from django.core.asgi import get_asgi_application
@@ -17,15 +18,18 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 import backend.route
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp.settings')
 django.setup()
 
 
 
 
-django_asgi_app = get_asgi_application()
+
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": get_asgi_application(),
     "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(
                 URLRouter(
