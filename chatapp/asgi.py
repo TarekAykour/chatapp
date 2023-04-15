121fc38a28d3cea44df231fbc1971280d3a0c9ef
TarekAykour <73133367+TarekAykour@users.noreply.github.com>
2023-04-15 14:07:53 +0200

    configured asgi with doc

--- chatapp/asgi.py
+++ chatapp/asgi.py
@@ -8,33 +8,29 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-import django
 
-
-
-import django
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
 import backend.route
+from django.urls import path
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp.settings')
-django.setup()
+django_asgi_app = get_asgi_application()
 
-
-
-
+from backend.consumer import ChatConsumer
 
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(
-                URLRouter(
-                backend.route.websocket_urlpatterns
-    )
+                URLRouter([
+                path('backend/', ChatConsumer.as_asgi())
+                # backend.route.websocket_urlpatterns
+    ])
     )
         ),
     # Just HTTP for now. (We can add other protocols later.)
